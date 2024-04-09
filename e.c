24cadaf0111afe85528955c537a0551388d537e1
admin <2076625002@qq.com>
2024-04-09 21:49:27 +0800

    sd

--- e.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
- int aa[2][4] = { 1, 2, 3, 4, 5, 6, 7, 8 };
- int *ptr1 = (int *)(&aa + 1);
- int *ptr2 = (int *)(*(aa + 1));
- printf( "%d,%d", *(ptr1 - 1), *(ptr2 - 1));
- return 0;
-}
--- f.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
- char *a[] = {"work","at","alibaba"};
- char**pa = a;
- pa++;
- printf("%s\n", *pa);
- return 0;
-}
